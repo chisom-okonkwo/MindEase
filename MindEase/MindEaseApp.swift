5339942e1bf3d1d94c1dfdbdae87c0dbda99fd3d
Chisom Bill <chisomokonkwo323@gmail.com>
2025-03-26 12:34:10 -0500

    Initial Commit

---
+++ MindEase/MindEaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindEaseApp.swift
+//  MindEase
+//
+//  Created by loccsmac4 on 3/26/25.
+//
+
+import SwiftUI
+
+@main
+struct MindEaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MindEaseTests/MindEaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  MindEaseTests.swift
+//  MindEaseTests
+//
+//  Created by loccsmac4 on 3/26/25.
+//
+
+import Testing
+@testable import MindEase
+
+struct MindEaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
